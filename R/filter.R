0ff9cc9e5d34f6fb271696ce4b566558c48db882
Mikko Marttila <mikko.marttila@orionpharma.com>
2018-06-13 14:27:38 +0300

    Improve negate_expr() readability in exclude()
    Rather than simply wrapping expressions in `!`, logic calls are negated following De Morgan's rule for `&` and `|`, and comparison operators are changed to the complement equivalent, e.g. `>` negated becomes `<=`.

--- R/filter.R
+++ R/filter.R
@@ -61,6 +61,52 @@ conds_negate <- function(...) {
   purrr::map(conds, expr_negate)
 }
 
-expr_negate <- function(expr) {
-  rlang::expr(!(!!expr))
+expr_negate <- function(x) {
+  if (!is_logic_call(x)) {
+    return(rlang::lang("!", x))
+  }
+
+  fn <- as.character(x[[1L]])
+  args <- x[-1L]
+
+  if (fn == "!") {
+    return(args[[1L]])
+  }
+
+  fn <- switch_logic_fn(fn)
+
+  if (fn %in% c("&", "|", "&&", "||")) {
+    # De Morgan: negate arguments, recursively
+    args <- lapply(args, expr_negate)
+  }
+
+  rlang::lang(fn, !!!args)
+}
+
+is_logic_call <- function(x) {
+  if (!is.call(x)) {
+    return(FALSE)
+  }
+
+  fn <- as.character(x[[1L]])
+
+  logic <- c("!", "&", "|", "&&", "||")
+  comp <- c("==", "!=", ">", ">=", "<", "<=")
+
+  fn %in% c(logic, comp)
+}
+
+switch_logic_fn <- function(x) {
+  switch(x,
+     "&" = "|",
+     "|" = "&",
+    "&&" = "||",
+    "||" = "&&",
+    "==" = "!=",
+    "!=" = "==",
+     ">" = "<=",
+    ">=" = "<",
+     "<" = ">=",
+    "<=" = ">"
+  )
 }
