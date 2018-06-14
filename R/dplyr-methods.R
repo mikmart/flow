9d578cf0ba6f06ff7e91cd5b38e7e677a3ed82af
Mikko Marttila <mikko.marttila@orionpharma.com>
2018-06-14 16:32:54 +0300

    Implement group_by.flow_df()
    First pass: will probably need to have an update_flow() added, and implementation of inheritance needs to be considered.

--- R/dplyr-methods.R
+++ R/dplyr-methods.R
@@ -3,3 +3,8 @@ filter.flow_df <- function(.data, ...) {
   included <- reconstruct(NextMethod(), .data)
   update_flow(included, .data, conds_text(...))
 }
+
+#' @export
+group_by.flow_df <- function(.data, ..., add = FALSE) {
+  as_flow(NextMethod(), flow = get_flow(.data))
+}
