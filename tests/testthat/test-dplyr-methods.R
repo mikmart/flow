ff623f87c33ea83503e3a9c265f40058878d0093
Mikko Marttila <mikmart@mikkomarttila.com>
2018-08-03 16:56:12 +0300

    More dplyr verb class tests

--- tests/testthat/test-dplyr-methods.R
+++ tests/testthat/test-dplyr-methods.R
@@ -1,6 +1,6 @@
 context("test-dplyr-methods.R")
 
-test_that("dplyr functions preserve flow_df", {
+test_that("verbs preserve flow_df", {
   df <- flow(data.frame(x = c(1, 2, 2), y = 6:4))
   grouped <- dplyr::group_by(df, x)
 
@@ -11,11 +11,16 @@ test_that("dplyr functions preserve flow_df", {
   expect_is(dplyr::arrange(df, y), "flow_df")
   expect_is(dplyr::group_by(df, x), "flow_df")
   expect_is(dplyr::transmute(df, z = 7), "flow_df")
+  expect_is(dplyr::add_count(df), "flow_df")
+  expect_is(dplyr::add_tally(df, x), "flow_df")
 
   expect_is(dplyr::filter(grouped, mean(y) < 6), "flow_df")
   expect_is(dplyr::mutate(grouped, z = mean(y)), "flow_df")
-  expect_is(dplyr::add_count(grouped, x), "flow_df")
+  expect_is(dplyr::add_count(grouped), "flow_df")
+  expect_is(dplyr::add_tally(grouped, x), "flow_df")
+})
 
+test_that("aggregations don't preserve flow_df", {
   expect_failure({
     expect_is(dplyr::count(df, x), "flow_df")
     expect_is(dplyr::tally(grouped), "flow_df")
