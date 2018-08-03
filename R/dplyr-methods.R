5ad25f9b6d7ada6529a2678ca87b81648c1fee38
Mikko Marttila <mikmart@mikkomarttila.com>
2018-08-03 16:44:01 +0300

    Add arrange() method

--- R/dplyr-methods.R
+++ R/dplyr-methods.R
@@ -3,3 +3,8 @@ filter.flow_df <- function(.data, ...) {
   included <- reconstruct(NextMethod(), .data)
   update_flow(included, .data, conds_text(...))
 }
+
+#' @export
+arrange.flow_df <- function(.data, ...) {
+  reconstruct(NextMethod(), .data)
+}
