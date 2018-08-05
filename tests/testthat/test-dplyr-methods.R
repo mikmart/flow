17b43d29de7e8b12704a69629b9224bdd6c47de2
Mikko Marttila <mikmart@mikkomarttila.com>
2018-08-05 16:31:29 +0300

    Separate grouped tests

--- tests/testthat/test-dplyr-methods.R
+++ tests/testthat/test-dplyr-methods.R
@@ -2,15 +2,19 @@ context("test-dplyr-methods.R")
 
 test_that("verbs preserve flow_df", {
   df <- flow(data.frame(x = c(1, 2, 2), y = 6:4))
-  grouped <- dplyr::group_by(df, x)
 
-  expect_is(dplyr::select(df, x), "flow_df")
+  expect_is(dplyr::arrange(df, y), "flow_df")
   expect_is(dplyr::filter(df, x > 1), "flow_df")
+  expect_is(dplyr::group_by(df, x), "flow_df")
   expect_is(dplyr::mutate(df, z = 7), "flow_df")
   expect_is(dplyr::rename(df, foo = x), "flow_df")
-  expect_is(dplyr::arrange(df, y), "flow_df")
-  expect_is(dplyr::group_by(df, x), "flow_df")
+  expect_is(dplyr::select(df, x), "flow_df")
   expect_is(dplyr::transmute(df, z = 7), "flow_df")
+})
+
+test_that("grouped verbs preserve flow_df", {
+  df <- flow(data.frame(x = c(1, 2, 2), y = 6:4))
+  grouped <- dplyr::group_by(df, x)
 
   expect_is(dplyr::filter(grouped, mean(y) < 6), "flow_df")
   expect_is(dplyr::mutate(grouped, z = mean(y)), "flow_df")
